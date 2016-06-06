4a24d84274bfd9591d3e2372e3bef23120be8741
zhaowei <1512763623@qq.com>
2016-06-06 23:44:56 +0800

    添加登录注册页面

--- src/js/common.js
+++ src/js/common.js
@@ -5,10 +5,22 @@ window.onload=function(){
 }
 
 function setContentHeight(){
+    var logFotter=document.getElementById("loginFotter");
     var header=document.getElementById("header").clientHeight;
-    var fotter=document.getElementById("fotter").clientHeight;
-    var contentHeight=document.body.clientHeight-header-fotter;
-    var content=document.getElementById("content");
 
-    content.style["min-height"]=contentHeight+"px"
+    if(logFotter){
+        var contentHeight=document.getElementById("content").clientHeight;
+        var fotterHeight=document.body.clientHeight-header-contentHeight;
+        if(fotterHeight<=90) return;
+        logFotter.style.height=fotterHeight+"px";
+        logFotter.style['line-height']=fotterHeight+"px";
+    }else{
+        var fotter=document.getElementById("fotter").clientHeight;
+        var contentHeight=document.body.clientHeight-header-fotter;
+        var content=document.getElementById("content");
+
+        content.style["min-height"]=contentHeight+"px"
+    }
+
+
 }
